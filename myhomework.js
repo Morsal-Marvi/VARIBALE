61fcd1b0aa431456a1f7db8f7ddcbeff89ac392e
Morsal-Marvi <161148886+Morsal-Marvi@users.noreply.github.com>
2024-05-21 09:34:23 -0700

    Add files via upload

---
+++ myhomework.js
@@ -0,0 +1,22 @@
+alert("hello welcome")
+//]variables
+let city = 'Tokyo';
+let region = 'Kanto';
+let population_density = 6150; 
+let area = 2188;
+
+//log
+console.log('City:', city);
+console.log('Region:', region);
+console.log('Population Density:', population_density, 'people per sq km');
+console.log('Area:', area, 'sq km');
+
+//Declare
+let isCapital = true;
+let officialLanguage; //not assign yet
+
+//Log the types
+console.log('Type of isCapital:', typeof isCapital);
+console.log('Type of population_density:', typeof population_density);
+console.log('Type of city:', typeof city);
+console.log('Type of officialLanguage:', typeof officialLanguage);
